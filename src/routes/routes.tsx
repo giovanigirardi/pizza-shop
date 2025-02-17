1d5b66696225fc639e5abe37f4c502ef00dba574
Giovani Girardi <giovanigg08@gmail.com>
2025-02-16 23:13:13 -0300

    feat: add AppLayout and AuthLayout for improved routing structure

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
+import { AppLayout } from "@/pages/_layouts/app";
+import { AuthLayout } from "@/pages/_layouts/auth";
+
 import { Dashboard } from "@/pages/app/dashboard";
 import { SignIn } from "@/pages/auth/sign-in";
 
@@ -7,8 +10,12 @@ export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Dashboard />} />
-        <Route path="/sign-in" element={<SignIn />} />
+        <Route element={<AppLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignIn />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
