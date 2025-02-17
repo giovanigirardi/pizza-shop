ae842380367ab68c5f819c927cb43bf7b807e38c
Giovani Girardi <giovanigg08@gmail.com>
2025-02-17 00:22:52 -0300

    feat: integrate react-hook-form and zod for form validation in SignIn component

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,30 @@
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Helmet } from "react-helmet-async";
+
+const signInFormSchema = z.object({
+  email: z.string().email(),
+});
+
+type SignInFormData = z.infer<typeof signInFormSchema>;
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInFormData>();
+
+  async function handleSignIn(data: SignInFormData) {
+    console.log(data);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -14,13 +35,13 @@ export function SignIn() {
             <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro</p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" placeholder="Digite seu e-mail" />
+              <Input id="email" type="email" placeholder="Digite seu e-mail" {...register("email")} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Acessar painel
             </Button>
           </form>
