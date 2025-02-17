91a817c5e372a0d53ecdf7c3df858e7f633f92e1
Giovani Girardi <giovanigg08@gmail.com>
2025-02-16 22:54:33 -0300

    feat: add Tailwind CSS setup and create reusable Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
-	return <p>Hello World</p>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p>Hello world!</p>
+      <Button>Click Me!</Button>
+    </div>
+  );
 }
